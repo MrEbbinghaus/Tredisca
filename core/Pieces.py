4bc04080bc8c1f8e74525b976b954873ef810003
Björn Ebbinghaus <bjoern.dpb@gmail.com>
2015-09-03 04:24:01 +0200

    close #2

--- core/Pieces.py
+++ core/Pieces.py
@@ -1,34 +1,25 @@
-from utilities import getRelVector
-from enum import Enum
+import utilities
 
 class Piece:
     def __init__(self, position, color):
         self.position = position
-        self.color = Color(color)
+        self.color = utilities.Color(color)
 
     def move(self, dest, board):
         if self.validDir(dest):
             if board.movePiece(self.position, dest) is None:
-                raise InvalidMoveException()
+                raise utilities.InvalidMoveException()
 
-            if type(self) in (Pawn, Rook, King):
+            if type(self) in (Rook, King):
                 self.unmoved = False
             self.position = dest
 
             print("{0} moved to {1}".format(self.name, dest))
         else:
-            raise InvalidMoveException()
+            raise utilities.InvalidMoveException()
 
     def getIcon(self):
         return self.icon
-
-class InvalidMoveException(Exception):
-    def __init__(self, message=""):
-        self.message = "Invalid Move!" + message
-
-class Color(Enum):
-    black = "black"
-    white = "white"
 
 class Pawn(Piece):
     name = "Pawn"
@@ -36,7 +27,7 @@ class Pawn(Piece):
 
     def __init__(self, position, color):
         self.position = position
-        self.color = Color(color)
+        self.color = utilities.Color(color)
         self.unmoved = True # needed for the double step
 
     def validDir(self, dest):
@@ -45,7 +36,7 @@ class Pawn(Piece):
         does not check if the destination is occupied or not on the board
         does check if a double step is possible
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
 
         if x is 0 and y is 0:
@@ -58,6 +49,25 @@ class Pawn(Piece):
         else:
             return False
 
+    def move(self, dest, board):
+        if self.validDir(dest):
+            (x, y, _) = utilities.getRelVector(self.position, dest)
+            if abs(x) is 1 or abs(y) is 1:
+                destPiece = board.getPiece(dest)
+                if destPiece is None or destPiece is 0:
+                    raise utilities.InvalidMoveException()
+
+            if board.movePiece(self.position, dest) is None:
+                raise utilities.InvalidMoveException()
+
+            if self.unmoved:
+                self.unmoved = False
+            self.position = dest
+
+            print("{0} moved to {1}".format(self.name, dest))
+        else:
+            raise utilities.InvalidMoveException()
+
 class Knight(Piece):
     name = "Knight"
     icon = "N"
@@ -67,7 +77,7 @@ class Knight(Piece):
         returns True, if the destination is a valid move
         does not check if the destination is occupied or not on the board
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
         if x is 0 and y is 0:
             return False
@@ -87,7 +97,7 @@ class Bishop(Piece):
         does not check if the destination is occupied or not on the board
         does not check if something is in the way
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
         if x is 0 and y is 0:
             return False
@@ -103,7 +113,7 @@ class Rook(Piece):
 
     def __init__(self, position, color):
         self.position = position
-        self.color = Color(color)
+        self.color = utilities.Color(color)
         self.unmoved = True # needed for castling
 
     def validDir(self, dest):
@@ -112,7 +122,7 @@ class Rook(Piece):
         does not check if the destination is occupied or not on the board
         does not check if something is in the way
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
         if (abs(x) is 0) ^ (abs(y) is 0):
             return True
@@ -129,7 +139,7 @@ class Queen(Piece):
         does not check if the destination is occupied or not on the board
         does not check if something is in the way
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
         if x is 0 and y is 0:
             return False
@@ -145,7 +155,7 @@ class King(Piece):
 
     def __init__(self, position, color):
         self.position = position
-        self.color = Color(color)
+        self.color = utilities.Color(color)
         self.unmoved = True # needed to know if the king has already moved
 
     def validDir(self, dest):
@@ -154,7 +164,7 @@ class King(Piece):
         does not check if the destination is occupied or not on the board
         does not check if a rochade is possible
         """
-        (x, y, _) = getRelVector(self.position, dest)
+        (x, y, _) = utilities.getRelVector(self.position, dest)
 
         if (abs(x) in (0, 1)) and (abs(y) in (0, 1)):
             return True
--- core/tredisca.py
+++ core/tredisca.py
@@ -1,82 +1,88 @@
 import pieces
-from utilities import Board
+from utilities import InvalidMoveException
+from board import Board
 from ast import literal_eval as make_tuple
 
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.initStartingPosition()
+        self.play()
+
+    def play(self):
+        print("How to play:\nExample: (x,y,z) to (x1,y1,z1)\nExample: KP0 to QP1")
+        while True:
+            self.board.prettyPrint()
+            doMove = True
+            while doMove:
+                (a, b) = input("White's Move:").split(" to ")
+                try:
+                    if a[0] in ("K", "Q"):
+                        self.board.moveAtkBoard(a, b)
+                    else:
+                        self.board.move(make_tuple(a), make_tuple(b))
+                    doMove = False
+                except InvalidMoveException:
+                    print("failed!")
+
+            while doMove:
+                (a, b) = input("Black's Move:").split(" to ")
+                try:
+                    if a[0] in ("K", "Q"):
+                        self.board.moveAtkBoard(a, b)
+                    else:
+                        self.board.move(make_tuple(a), make_tuple(b))
+                    doMove = False
+                except InvalidMoveException:
+                    print("failed!")
+
+    def initStartingPosition(self):
+        self.board.addAll((
+        pieces.Rook((0,0,1), "white"),
+        pieces.Queen((1,0,1), "white"),
+        pieces.Pawn((0,1,1), "white"),
+        pieces.Pawn((1,1,1), "white"),
+
+        pieces.Pawn((1,2,0), "white"),
+        pieces.Pawn((2,2,0), "white"),
+        pieces.Pawn((3,2,0), "white"),
+        pieces.Pawn((4,2,0), "white"),
+        pieces.Knight((1,1,0), "white"),
+        pieces.Bishop((2,1,0), "white"),
+        pieces.Bishop((3,1,0), "white"),
+        pieces.Knight((4,1,0), "white"),
+
+        pieces.King((4,0,1), "white"),
+        pieces.Rook((5,0,1), "white"),
+        pieces.Pawn((4,1,1), "white"),
+        pieces.Pawn((5,1,1), "white"),
+
+        pieces.Rook((0,9,5), "black"),
+        pieces.Queen((1,9,5), "black"),
+        pieces.Pawn((0,8,5), "black"),
+        pieces.Pawn((1,8,5), "black"),
+
+        pieces.Pawn((1,7,4), "black"),
+        pieces.Pawn((2,7,4), "black"),
+        pieces.Pawn((3,7,4), "black"),
+        pieces.Pawn((4,7,4), "black"),
+        pieces.Knight((1,8,4), "black"),
+        pieces.Bishop((2,8,4), "black"),
+        pieces.Bishop((3,8,4), "black"),
+        pieces.Knight((4,8,4), "black"),
+
+        pieces.King((4,9,5), "black"),
+        pieces.Rook((5,9,5), "black"),
+        pieces.Pawn((4,8,5), "black"),
+        pieces.Pawn((5,8,5), "black")
+        ))
+
+
 def main():
-    board = Board()
-    initStartingPosition(board)
-    #board.move((1,1,0), (2,3,2))
-    #print("++++++++++++++++")
-    #board.prettyPrint()
-    play(board)
+    Game()
 
-def initStartingPosition(board):
-    board.addAll((
-    pieces.Rook((0,0,1), "white"),
-    pieces.Queen((1,0,1), "white"),
-    pieces.Pawn((0,1,1), "white"),
-    pieces.Pawn((1,1,1), "white"),
 
-    pieces.Pawn((1,2,0), "white"),
-    pieces.Pawn((2,2,0), "white"),
-    pieces.Pawn((3,2,0), "white"),
-    pieces.Pawn((4,2,0), "white"),
-    pieces.Knight((1,1,0), "white"),
-    pieces.Bishop((2,1,0), "white"),
-    pieces.Bishop((3,1,0), "white"),
-    pieces.Knight((4,1,0), "white"),
 
-    pieces.King((4,0,1), "white"),
-    pieces.Rook((5,0,1), "white"),
-    pieces.Pawn((4,1,1), "white"),
-    pieces.Pawn((5,1,1), "white"),
-
-    pieces.Rook((0,9,5), "black"),
-    pieces.Queen((1,9,5), "black"),
-    pieces.Pawn((0,8,5), "black"),
-    pieces.Pawn((1,8,5), "black"),
-
-    pieces.Pawn((1,7,4), "black"),
-    pieces.Pawn((2,7,4), "black"),
-    pieces.Pawn((3,7,4), "black"),
-    pieces.Pawn((4,7,4), "black"),
-    pieces.Knight((1,8,4), "black"),
-    pieces.Bishop((2,8,4), "black"),
-    pieces.Bishop((3,8,4), "black"),
-    pieces.Knight((4,8,4), "black"),
-
-    pieces.King((4,9,5), "black"),
-    pieces.Rook((5,9,5), "black"),
-    pieces.Pawn((4,8,5), "black"),
-    pieces.Pawn((5,8,5), "black")
-    ))
-
-def play(board):
-    print("How to play:\nExample: (x,y,z) to (x1,y1,z1)\nExample: KP0 to QP1")
-    while True:
-        board.prettyPrint()
-        doMove = True
-        while doMove:
-            (a, b) = input("White's Move:").split(" to ")
-            try:
-                if a[0] in ("K", "Q"):
-                    board.moveAtkBoard(a, b)
-                else:
-                    board.move(make_tuple(a), make_tuple(b))
-                doMove = False
-            except pieces.InvalidMoveException:
-                print("failed!")
-
-        while doMove:
-            (a, b) = input("Black's Move:").split(" to ")
-            try:
-                if a[0] in ("K", "Q"):
-                    board.moveAtkBoard(a, b)
-                else:
-                    board.move(make_tuple(a), make_tuple(b))
-                doMove = False
-            except pieces.InvalidMoveException:
-                print("failed!")
 
 
 
